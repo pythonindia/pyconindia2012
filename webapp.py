ac41f717d4156a66603f21a131559633f49e28f1
kracekumar <kracethekingmaker@gmail.com>
2012-06-27 20:22:21 +0530

    added function to dynamically check the dates to strike in important dates

--- webapp.py
+++ webapp.py
@@ -11,7 +11,7 @@ app.register_blueprint(baseframe)
 
 assets = Environment(app)
 
-js = Bundle(baseframe_js, 'js/leaflet.js')
+js = Bundle(baseframe_js, 'js/leaflet.js', 'js/check_dates.js')
 
 css = Bundle(Bundle(baseframe_css, 'css/pyconindia.css', 'css/monitor.css',
              filters='cssmin', output='css/pyconindia-packed.css'),
@@ -22,7 +22,8 @@ assets.register('css_all', css)
 
 # Import models and views, and register routes here
 
-from flask import render_template, redirect, url_for
+from flask import render_template
+
 
 @app.route('/')
 def index():
